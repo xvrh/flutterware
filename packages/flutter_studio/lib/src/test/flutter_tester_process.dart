8be2fe24db7ef902a116275e0d6d65c0d83412b7
Xavier Hainaux <xavier.hainaux@gmail.com>
2022-05-06 19:36:47 +0200

    .

--- packages/flutter_studio/lib/src/test/flutter_tester_process.dart
+++ packages/flutter_studio/lib/src/test/flutter_tester_process.dart
@@ -1,17 +1,27 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'package:logging/logging.dart';
 
 final _logger = Logger('flutter_tester_process');
 
+const _toolStartedSignal =
+    'The Flutter DevTools debugger and profiler on Flutter test device is available at';
+
 class FlutterTesterProcess {
   final Process _process;
+  final _onStartedCompleter = Completer<void>();
 
   FlutterTesterProcess._(this._process) {
     _process.stdout
         .transform(Utf8Decoder())
         .transform(const LineSplitter())
         .listen((line) {
+      if (line.startsWith(_toolStartedSignal) &&
+          !_onStartedCompleter.isCompleted) {
+        _onStartedCompleter.complete();
+      }
+
       _logger.fine(line);
     });
     _process.stderr
@@ -28,5 +38,23 @@ class FlutterTesterProcess {
         'flutter', ['run', '-d', 'flutter-tester'],
         workingDirectory: directory.path);
     return FlutterTesterProcess._(process);
+  }
+
+  Future<void> get onStarted => _onStartedCompleter.future;
+  Future<void> get onExit => _process.exitCode;
+
+  Future<void> reload() async {
+    _process.stdin.write('r');
+    await Future.delayed(const Duration(seconds: 1));
+  }
+
+  Future<void> restart() async {
+    _process.stdin.write('R');
+    await Future.delayed(const Duration(seconds: 2));
+  }
+
+  Future<void> quit() async {
+    _process.stdin.write('q');
+    return onExit;
   }
 }
